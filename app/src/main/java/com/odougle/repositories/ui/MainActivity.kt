91dcf3c88073bc29da0dc5098c8861cafc08b590
douguizilla <douglasgommez@gmail.com>
2021-07-24 14:06:10 -0300

    set viewBinding on MainActivity.kt and create a menu to put search on a toolbar

--- app/src/main/java/com/odougle/repositories/ui/MainActivity.kt
+++ app/src/main/java/com/odougle/repositories/ui/MainActivity.kt
@@ -2,11 +2,23 @@ package com.odougle.repositories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.odougle.repositories.R
+import com.odougle.repositories.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
