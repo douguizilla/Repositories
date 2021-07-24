bf8bb83c29024bc57346c0b73f61b892541247b8
douguizilla <douglasgommez@gmail.com>
2021-07-24 16:27:53 -0300

    define Use case to list user repositories and start do create dependency injection for Domains

---
+++ app/src/main/java/com/odougle/repositories/domain/ListUserRepositoriesUseCase.kt
@@ -0,0 +1,14 @@
+package com.odougle.repositories.domain
+
+import com.odougle.repositories.core.UseCase
+import com.odougle.repositories.data.model.Repo
+import com.odougle.repositories.data.repositories.RepoRepository
+import kotlinx.coroutines.flow.Flow
+
+class ListUserRepositoriesUseCase(
+    private val repository: RepoRepository
+): UseCase<String, List<Repo>>() {
+    override suspend fun execute(param: String): Flow<List<Repo>> {
+        return repository.listRepositories(param)
+    }
+}
---
+++ app/src/main/java/com/odougle/repositories/domain/di/DomainModule.kt
@@ -0,0 +1,4 @@
+package com.odougle.repositories.domain.di
+
+object DomainModule {
+}
