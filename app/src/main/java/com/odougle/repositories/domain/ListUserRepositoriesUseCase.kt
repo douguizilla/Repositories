b75440ef7505be1b9dacb8633e1a525eb06d99bc
douguizilla <douglasgommez@gmail.com>
2021-07-24 16:31:53 -0300

    set DomainModule.kt

--- app/src/main/java/com/odougle/repositories/domain/ListUserRepositoriesUseCase.kt
+++ app/src/main/java/com/odougle/repositories/domain/ListUserRepositoriesUseCase.kt
@@ -6,7 +6,8 @@ import com.odougle.repositories.data.repositories.RepoRepository
 import kotlinx.coroutines.flow.Flow
 
 class ListUserRepositoriesUseCase(
-    private val repository: RepoRepository
+    private val repository: RepoRepository,
+
 ): UseCase<String, List<Repo>>() {
     override suspend fun execute(param: String): Flow<List<Repo>> {
         return repository.listRepositories(param)
--- app/src/main/java/com/odougle/repositories/domain/di/DomainModule.kt
+++ app/src/main/java/com/odougle/repositories/domain/di/DomainModule.kt
@@ -1,4 +1,21 @@
 package com.odougle.repositories.domain.di
 
+import com.odougle.repositories.domain.ListUserRepositoriesUseCase
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
 object DomainModule {
+    fun load(){
+        loadKoinModules(useCaseModule())
+    }
+
+    private fun useCaseModule(): Module {
+        return module {
+            //always return a new instance for this use case
+            factory {
+                ListUserRepositoriesUseCase(get())
+            }
+        }
+    }
 }
